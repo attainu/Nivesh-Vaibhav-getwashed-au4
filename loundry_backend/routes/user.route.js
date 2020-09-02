df8ed22e542c5cb2b0b4e43f49c427d7db5809c3
vaibhav <vaibhavdeshmukh0703@gmail.com>
2020-09-02 19:31:42 +0530

    mail

--- loundry_backend/routes/user.route.js
+++ loundry_backend/routes/user.route.js
@@ -76,12 +76,13 @@ router.post('/signup',async(req, res, next)=> {
           // })
         
           let mailResult = await smtpTransport.sendMail(mailOption);
-          if(mailResult){
-            res.status(200).json({ 'message':'Check your Email for verification. Click the link given below'})
-          }
-          else{
-            console.log(error);
-          }
+          res.status(200).json({ 'message':'Check your Email for verification. Click the link given below'})
+          // if(mailResult){
+          //   res.status(200).json({ 'message':'Check your Email for verification. Click the link given below'})
+          // }
+          // else{
+          //   console.log(error);
+          // }
 
       }
       else{
